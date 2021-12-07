e0757eb9e0dafc37b96e2c6fc818f6b40049507e
Julian Hernandez <julian@julianhernandez.me>
2021-12-07 17:34:57 -0500

    Init commit

---
+++ visual-editor-dark-theme.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package Visual Editor Dark Theme
+ * Plugin Name: Visual Editor Dark Theme
+ * Plugin URI: https://metainteractive.com
+ * Description: Enable dark mode on visual editor
+ * Version: 1.0.0
+ * Author: Meta Interactive
+ * Author URI: https://metainteractetive.com
+ * License: GPLv2 or later
+ * Text Domain: vdmt
+ */
+
+/* Add Editor Style */
+add_filter( 'mce_css', 'my_plugin_editor_style' );
+
+function my_plugin_editor_style( $mce_css ){
+    $mce_css .= ', ' . plugins_url( 'vedt-visual-tab.css', __FILE__ );
+    return $mce_css;
+}
+
+function vedt_admin_style() {
+  wp_enqueue_style('vedt-text-tab', plugin_dir_url( __FILE__ ) . 'vedt-text-tab.css');
+}
+add_action('admin_enqueue_scripts', 'vedt_admin_style');
+    
