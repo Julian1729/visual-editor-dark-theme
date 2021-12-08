c072021e2a09b16907283ace5d661b69b8da8cdf
Julian Hernandez <julian@julianhernandez.me>
2021-12-07 23:21:59 -0500

    Refactor to only activate for a hardcoded user id

--- visual-editor-dark-theme.php
+++ visual-editor-dark-theme.php
@@ -11,16 +11,26 @@
  * Text Domain: vdmt
  */
 
-/* Add Editor Style */
-add_filter( 'mce_css', 'my_plugin_editor_style' );
+function vedt_init() {
+  // NOTE: user id to enable for
+  $user_id = 2;
 
-function my_plugin_editor_style( $mce_css ){
+  if(get_current_user_id() !== 2) return;
+
+  /* Add Editor Style */
+  add_filter( 'mce_css', 'my_plugin_editor_style' );
+
+  function my_plugin_editor_style( $mce_css ){
     $mce_css .= ', ' . plugins_url( 'vedt-visual-tab.css', __FILE__ );
     return $mce_css;
+  }
+
+  function vedt_admin_style() {
+    wp_enqueue_style('vedt-text-tab', plugin_dir_url( __FILE__ ) . 'vedt-text-tab.css');
+  }
+
+  add_action('admin_enqueue_scripts', 'vedt_admin_style');
+
 }
 
-function vedt_admin_style() {
-  wp_enqueue_style('vedt-text-tab', plugin_dir_url( __FILE__ ) . 'vedt-text-tab.css');
-}
-add_action('admin_enqueue_scripts', 'vedt_admin_style');
-    
+add_action('init', 'vedt_init');
